a6a17e2fca6992dcf9122f1e44d4d47d63127c11
Etea <eteaisme@gmail.com>
2024-02-20 17:03:28 -0500

    Implement Intake Subsystem

--- src/main/java/ca/warp7/subsystems/Intake/intakeIO.java
+++ src/main/java/ca/warp7/subsystems/Intake/intakeIO.java
@@ -1,9 +1,13 @@
 package ca.warp7.subsystems.Intake;
 import org.littletonrobotics.junction.AutoLog;
-
 public interface intakeIO {
     @AutoLog
     public static class intakeIOInputs {
+        public double intakeAppliedVolts = 0.0;
+        public double[] intakeCurrentAmps = new double []{}; 
     }
-    public void setIntakeSpeed(double volts);
+    /*Updates the set of loggable inputs*/
+    public void updateInputs(intakeIOInputs inputs);
+    /*Set the intake voltage */
+    public void setIntakeVoltage(double volts);
 }
