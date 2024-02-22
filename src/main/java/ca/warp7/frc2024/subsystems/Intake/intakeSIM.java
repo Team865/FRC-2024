9e02af13bf0909ad107a1cdb8be66009319b99d4
Etea <eteaisme@gmail.com>
2024-02-22 16:12:30 -0500

    Format robot code

--- src/main/java/ca/warp7/frc2024/subsystems/Intake/intakeSIM.java
+++ src/main/java/ca/warp7/frc2024/subsystems/Intake/intakeSIM.java
@@ -1,16 +1,19 @@
 package ca.warp7.frc2024.subsystems.Intake;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
-public class intakeSIM  implements intakeIO{
+public class intakeSIM implements intakeIO {
     private double intakeAppliedVolts = 0.0;
-    private final DCMotorSim intakeMotorSim = new DCMotorSim(DCMotor.getNEO(1), 4,0.0001);
+    private final DCMotorSim intakeMotorSim = new DCMotorSim(DCMotor.getNEO(1), 4, 0.0001);
+
     @Override
     public void updateInputs(intakeIOInputs inputs) {
         inputs.intakeAppliedVolts = intakeAppliedVolts;
-        inputs.intakeCurrentAmps = new double[]{};
+        inputs.intakeCurrentAmps = new double[] {};
     }
+
     @Override
     public void setIntakeVoltage(double volts) {
         intakeAppliedVolts = MathUtil.clamp(volts, -12, 12);
