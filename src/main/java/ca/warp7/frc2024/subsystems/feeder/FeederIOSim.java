632e469841253acd9fcf61dc16cc380eb359284e
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2024-02-23 17:41:27 -0500

    Implement feeder

--- src/main/java/ca/warp7/frc2024/subsystems/feeder/FeederIOSim.java
+++ src/main/java/ca/warp7/frc2024/subsystems/feeder/FeederIOSim.java
@@ -5,34 +5,34 @@ import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
 public class FeederIOSim implements FeederIO {
-    private DCMotorSim topFeederSim = new DCMotorSim(DCMotor.getNeo550(1), 5, 0.0001);
-    private DCMotorSim bottomFeederSim = new DCMotorSim(DCMotor.getNeo550(1), 5, 0.0001);
-    private double topFeederVoltsApplied = 0.0;
-    private double bottomFeederVoltsApplied = 0.0;
+    private DCMotorSim topRollerSim = new DCMotorSim(DCMotor.getNeo550(1), 5, 0.0001);
+    private DCMotorSim bottomRollerSim = new DCMotorSim(DCMotor.getNeo550(1), 5, 0.0001);
+    private double topRollerVoltsApplied = 0.0;
+    private double bottomRollerVoltsApplied = 0.0;
 
     @Override
     public void updateInputs(FeederIOInputs inputs) {
-        inputs.topFeederRoller.VelocityRad = this.topFeederSim.getAngularVelocityRadPerSec();
-        inputs.topFeederRoller.CurrentDraw = this.topFeederSim.getCurrentDrawAmps();
-        inputs.topFeederRoller.VoltageApplied = this.topFeederVoltsApplied;
+        this.topRollerSim.update(0.2);
+        this.bottomRollerSim.update(0.2);
 
-        inputs.bottomFeederRoller.VelocityRad = this.bottomFeederSim.getAngularVelocityRadPerSec();
-        inputs.bottomFeederRoller.CurrentDraw = this.bottomFeederSim.getCurrentDrawAmps();
-        inputs.bottomFeederRoller.VoltageApplied = this.bottomFeederVoltsApplied;
+        inputs.topRoller.VelocityRPM = this.topRollerSim.getAngularVelocityRadPerSec();
+        inputs.topRoller.CurrentDraw = this.topRollerSim.getCurrentDrawAmps();
+        inputs.topRoller.VoltsApplied = this.topRollerVoltsApplied;
+
+        inputs.bottomRoller.VelocityRPM = this.bottomRollerSim.getAngularVelocityRadPerSec();
+        inputs.bottomRoller.CurrentDraw = this.bottomRollerSim.getCurrentDrawAmps();
+        inputs.bottomRoller.VoltsApplied = this.bottomRollerVoltsApplied;
     }
 
     @Override
-    public void setFeederRollersVoltage(double volts) {
-        this.topFeederVoltsApplied = MathUtil.clamp(volts, -12, 12);
-        this.topFeederSim.setInputVoltage(this.topFeederVoltsApplied);
-
-        this.bottomFeederVoltsApplied = MathUtil.clamp(-volts, -12, 12);
-        this.bottomFeederSim.setInputVoltage(this.bottomFeederVoltsApplied);
+    public void setTopVoltage(double volts) {
+        this.topRollerVoltsApplied = MathUtil.clamp(volts, -12, 12);
+        this.topRollerSim.setInputVoltage(this.topRollerVoltsApplied);
     }
 
     @Override
-    public void periodic() {
-        this.topFeederSim.update(0.2);
-        this.bottomFeederSim.update(0.2);
+    public void setBottomVoltage(double volts) {
+        this.bottomRollerVoltsApplied = MathUtil.clamp(-volts, -12, 12);
+        this.bottomRollerSim.setInputVoltage(this.bottomRollerVoltsApplied);
     }
 }
