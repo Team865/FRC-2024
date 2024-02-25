38b73bc0403d68afcca25bac3dffced6caccc449
Dangosaurus <hey@dangosaurus.com>
2024-02-25 02:41:08 -0500

    Fix ShooterIO naming consistency

--- src/main/java/ca/warp7/frc2024/subsystems/shooter/ShooterIO.java
+++ src/main/java/ca/warp7/frc2024/subsystems/shooter/ShooterIO.java
@@ -3,18 +3,11 @@ package ca.warp7.frc2024.subsystems.shooter;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface ShooterIO {
+    @AutoLog
     public static class ShooterIOInputs {
-        @AutoLog
-        public static class OutRunnerIOInputs {
-            public double VelocityRPM = 0.0;
-            public double VoltsApplied = 0.0;
-            public double CurrentDraw = 0.0;
-        }
-
-        public OutRunnerIOInputsAutoLogged topRight = new OutRunnerIOInputsAutoLogged();
-        public OutRunnerIOInputsAutoLogged bottomRight = new OutRunnerIOInputsAutoLogged();
-        public OutRunnerIOInputsAutoLogged topLeft = new OutRunnerIOInputsAutoLogged();
-        public OutRunnerIOInputsAutoLogged bottomLeft = new OutRunnerIOInputsAutoLogged();
+        public double shooterVelocityRadPerSec = 0.0;
+        public double shooterAppliedVolts = 0.0;
+        public double shooterCurrentAmps = 0.0;
     }
 
     /** Updates the set of loggable inputs.
@@ -23,25 +16,25 @@ public interface ShooterIO {
      */
     public void updateInputs(ShooterIOInputs inputs);
 
-    /** Run the top right outrunner at the specified voltage;
+    /** Run the top right shooter at the specified voltage;
      *
      * @param volts
      */
     public void setTopRightVoltage(double volts);
 
-    /** Run the top left outrunner at the specified voltage;
+    /** Run the top left shooter at the specified voltage;
      *
      * @param volts
      */
     public void setTopLeftVoltage(double volts);
 
-    /** Run the bottom right outrunner at the specified voltage;
+    /** Run the bottom right shooter at the specified voltage;
      *
      * @param volts
      */
     public void setBottomRightVoltage(double volts);
 
-    /** Run the bottom left outrunner at the specified voltage;
+    /** Run the bottom left shooter at the specified voltage;
      *
      * @param volts
      */
