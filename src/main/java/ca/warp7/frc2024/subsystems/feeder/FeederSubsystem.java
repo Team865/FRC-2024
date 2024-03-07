c9ef63f2182a0445507e679976156a445d90e084
Dangosaurus <hey@dangosaurus.com>
2024-03-07 12:55:23 -0500

    Remove feeder controllers

--- src/main/java/ca/warp7/frc2024/subsystems/feeder/FeederSubsystem.java
+++ src/main/java/ca/warp7/frc2024/subsystems/feeder/FeederSubsystem.java
@@ -1,71 +1,39 @@
 package ca.warp7.frc2024.subsystems.feeder;
 
-import ca.warp7.frc2024.Constants;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import lombok.Setter;
 import org.littletonrobotics.junction.Logger;
 
 public class FeederSubsystem extends SubsystemBase {
     private final FeederIO io;
     private final FeederIOInputsAutoLogged inputs = new FeederIOInputsAutoLogged();
-    private final SimpleMotorFeedforward feedforward;
-    private final PIDController feedback;
 
-    private Double velocity;
-    private double volts;
+    @Setter
+    private double volts = 0;
 
-    public FeederSubsystem(FeederIO io) {
-        this.io = io;
-
-        switch (Constants.CURRENT_MODE) {
-            case REAL:
-                feedforward = new SimpleMotorFeedforward(0, 0, 0);
-                feedback = new PIDController(0.0, 0.0, 0.0);
-                break;
-            case SIM:
-                feedforward = new SimpleMotorFeedforward(0, 0, 0);
-                feedback = new PIDController(0.0, 0.0, 0.0);
-                break;
-            default:
-                feedforward = new SimpleMotorFeedforward(0, 0, 0);
-                feedback = new PIDController(0.0, 0.0, 0.0);
-                break;
-        }
+    public FeederSubsystem(FeederIO feederIO) {
+        this.io = feederIO;
     }
 
-    public void setRPM(double RPM) {
-        volts = 0;
-        velocity = Units.rotationsPerMinuteToRadiansPerSecond(RPM);
-    }
-
-    public void setVolts(double volts) {
-        velocity = null;
-        this.volts = volts;
-    }
-
-    public Command runVolts(double volts) {
-        return Commands.startEnd(() -> setVolts(volts), () -> setVolts(0), this);
-    }
-
-    public boolean getSensor() {
+    private boolean getSensor() {
         return inputs.feederSensorTriggered;
+    }
+
+    public Command runVoltage(double volts) {
+        return this.startEnd(() -> setVolts(volts), () -> setVolts(0));
+    }
+
+    public Trigger sensorTrigger() {
+        return new Trigger(() -> getSensor()).debounce(0.075);
     }
 
     @Override
     public void periodic() {
         io.updateInputs(inputs);
+        Logger.processInputs("Feeder", inputs);
 
-        Logger.processInputs("Feeder/Feeder", inputs);
-
-        if (velocity != null) {
-            double voltage = feedback.calculate(inputs.feederVelocityRadPerSec, velocity);
-            io.setVoltage(voltage);
-        } else {
-            io.setVoltage(volts);
-        }
+        io.setVoltage(volts);
     }
 }
