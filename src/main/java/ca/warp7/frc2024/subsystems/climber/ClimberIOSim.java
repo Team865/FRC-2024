b10e426f43e5f4b25cf0a9957750a06cd97e009c
Dangosaurus <hey@dangosaurus.com>
2024-03-09 04:06:17 -0500

    Fix crash when simulating since ClimberSim isn't implemented

--- src/main/java/ca/warp7/frc2024/subsystems/climber/ClimberIOSim.java
+++ src/main/java/ca/warp7/frc2024/subsystems/climber/ClimberIOSim.java
@@ -1,3 +1,11 @@
 package ca.warp7.frc2024.subsystems.climber;
 
-public class ClimberIOSim implements ClimberIO {}
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class ClimberIOSim implements ClimberIO {
+
+    @Override
+    public void updateInputs(ClimberIOInputs inputs) {
+        inputs.climberInternalPosition = new Rotation2d();
+    }
+}
